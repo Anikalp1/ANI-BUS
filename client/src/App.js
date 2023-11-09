c34e51e3e234a2cfc7bf15c3e4445d5e15621642
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2023-11-09 12:48:39 +0530

    Modified App.js, added home.js, register.js, login.js, auth.css and global.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import {Button} from 'antd';
 import 'antd/dist/reset.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className="App m-5">
-      <h1> ANI-BUS </h1>
-      <Button type="primary">Primary Button</Button>
-
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
