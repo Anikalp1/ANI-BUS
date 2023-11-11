da58a2666d94691335fcc269edc9ac8add4d0466
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2023-11-11 16:42:53 +0530

    Completed front-end and back-end part of registration page

---
+++ routes/usersRoute.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const User = require('../models/usersModel');
+const bcrypt = require("bcryptjs");
+//register new user
+router.post('/register', async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email })
+    if (existingUser) {
+      return res.send({
+        message: 'User Already Exists',
+        success: false,
+        data: null,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    req.body.password = hashedPassword;
+    const newUser = await new User(req.body)
+    await newUser.save();
+    res.send({
+      message: "User created successfully",
+      success: true,
+      data: null,
+    });
+  } catch (error) {
+    res.send({
+      message: error.message,
+      success: false,
+      data: null,
+    });
+  }
+});
+
+module.exports = router
