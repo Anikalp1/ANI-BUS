f91aa9fd482d8c0ff62426728c09bb399444974e
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2023-11-23 17:05:14 +0530

    Implemented whole booking feature

--- client/src/pages/BookNow.js
+++ client/src/pages/BookNow.js
@@ -4,8 +4,10 @@ import { useDispatch } from "react-redux";
 import { Col, Row, message } from "antd";
 import { axiosInstance } from "../helpers/axiosInstance";
 import { useParams } from "react-router-dom";
+import SeatSelection from "../components/SeatSelection";
 
 function BookNow() {
+  const [selectedSeats, setSelectedSeats] = useState([]);
   const params = useParams();
   const dispatch = useDispatch();
   const [bus, setBus] = useState(null);
@@ -33,22 +35,47 @@ function BookNow() {
   return (
     <div>
       {bus && (
-        <Row className="mt-3">
+        <Row className="mt-3" gutter={20}>
           <Col lg={12} xs={24} sm={24}>
             <h1 className="text-2xl text-secondary">{bus.name}</h1>
-            <h1 className="text-lg">{bus.from} - {bus.to} </h1>
+            <h1 className="text-lg">
+              {bus.from} - {bus.to}{" "}
+            </h1>
             <hr />
 
-            <div>
-                <h1 className="text-lg"><b>Journey Date</b> : {bus.journeyDate}</h1>
-                <h1 className="text-lg"><b>Fare</b> : ₹ {bus.fare} /- </h1>
-                <h1 className="text-lg"><b>Departure Time</b> : {bus.departure}</h1>
-                <h1 className="text-lg"><b>Arrival Time</b> : {bus.arrival}</h1>
+            <div className="flex flex-col gap-1">
+              <h1 className="text-lg">
+                <b>Journey Date</b> : {bus.journeyDate}
+              </h1>
+              <h1 className="text-lg">
+                <b>Fare</b> : ₹ {bus.fare} /-{" "}
+              </h1>
+              <h1 className="text-lg">
+                <b>Departure Time</b> : {bus.departure}
+              </h1>
+              <h1 className="text-lg">
+                <b>Arrival Time</b> : {bus.arrival}
+              </h1>
+            </div>
+            <hr />
 
+            <div className="flex flex-col gap-1">
+              <h1 className="text-xl">
+                <b>Selected Seats</b> : {selectedSeats.join(", ")}
+              </h1>
+              <h1 className="text-xl mt-2"><b>Fare : </b> ₹ {bus.fare * selectedSeats.length} /-</h1>
+
+              <button className="secondary-btn mt-3">Book Now</button>
             </div>
           </Col>
 
-          <Col lg={12} xs={24} sm={24}></Col>
+          <Col lg={12} xs={24} sm={24}>
+            <SeatSelection
+              selectedSeats={selectedSeats}
+              setSelectedSeats={setSelectedSeats}
+              bus={bus}
+            />
+          </Col>
         </Row>
       )}
     </div>
