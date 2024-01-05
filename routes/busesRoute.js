c45585d0070cec8e07195f4bcc454f34aed9c336
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2024-01-05 22:25:00 +0530

    Now users can filter through buses

--- routes/busesRoute.js
+++ routes/busesRoute.js
@@ -25,44 +25,39 @@ router.post("/add-bus", async (req, res) => {
 });
 
 //update-bus
-router.post("/update-bus", authMiddleware, async(req,res) => {
-  try{
+router.post("/update-bus", authMiddleware, async (req, res) => {
+  try {
     await Bus.findByIdAndUpdate(req.body._id, req.body);
     return res.status(200).send({
       success: true,
       message: "Bus updated successfully",
     });
-  }
-  catch(error)
-  {
+  } catch (error) {
     res.status(500).send({ success: false, message: error.message });
   }
 });
 
-
 //delete-bus
-router.post("/delete-bus", authMiddleware, async(req, res) => {
-  try
-  {
+router.post("/delete-bus", authMiddleware, async (req, res) => {
+  try {
     await Bus.findByIdAndDelete(req.body._id);
     return res.status(200).send({
       success: true,
       message: "Bus deleted successfully",
     });
-  }
-  catch(error){
+  } catch (error) {
     res.status(500).send({
       success: false,
-      message: error.message
+      message: error.message,
     });
-    }
+  }
 });
 
 //get-all-buses
 
 router.post("/get-all-buses", authMiddleware, async (req, res) => {
   try {
-    const buses = await Bus.find();
+    const buses = await Bus.find(req.body.filters);
     return res.status(200).send({
       success: true,
       message: "Buses fetched successfully",
@@ -77,7 +72,7 @@ router.post("/get-all-buses", authMiddleware, async (req, res) => {
 });
 
 //get-bus-by-id
-router.post("/get-bus-by-id", authMiddleware, async(req, res) => {
+router.post("/get-bus-by-id", authMiddleware, async (req, res) => {
   try {
     const bus = await Bus.findById(req.body._id);
     return res.status(200).send({
@@ -88,9 +83,9 @@ router.post("/get-bus-by-id", authMiddleware, async(req, res) => {
   } catch (error) {
     res.status(500).send({
       success: false,
-      message: error.message
+      message: error.message,
     });
   }
-})
+});
 
 module.exports = router;
