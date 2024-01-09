e522b38068ede2445356894dd1f7b843022ee10c
Anikalp Jaiswal <Anikalpjaiswal1@gmail.com>
2024-01-09 14:16:37 +0530

    Minor Fixes

--- server/server.js
+++ server/server.js
@@ -7,8 +7,9 @@ const corsOptions = {
     credentials: true,
     origin: ["http://localhost:3000/"],
   };
-app.use(cors(corsOptions));
+
 const app = express();
+app.use(cors(corsOptions));
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 app.use(express.json())
